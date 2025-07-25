61b5d65b7e1d25c93cee7664601d663a66eb64e1
zhouyiqi <zhouyiqi@singleronbio.com>
2025-07-25 16:06:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+Small est data for MobiuSCOPE(3' + 5' scRNA-Seq)
+Pipeline: https://github.com/singleron-RD/celescope-mobiu/
---
+++ human_normal_adjacent_tissue/run_shell.sh
@@ -0,0 +1,5 @@
+multi_mobiu \
+ --mapfile mapfile.tsv\
+ --genomeDir /SGRNJ06/randd/USER/zhouyiqi/genome/rna/celescope2.0.0/hs\
+ --kbDir /SGRNJ06/randd/public/genome/kb/hs_ensembl99/ \
+ --mod shell
